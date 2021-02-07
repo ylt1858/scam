4c33d799557b1ccac0994be256e7c340d5236c63
ylt <2262018714@qq.com>
2021-02-07 21:14:49 +0800

    add

--- src/main/java/scam/controller/AwardWinningController.java
+++ src/main/java/scam/controller/AwardWinningController.java
@@ -30,4 +30,41 @@ public class AwardWinningController {
         awardWinningMapper.deleteModel(id);
         return "redirect:/AWTable";
     }
+
+    @RequestMapping("/addAwardWinning")
+    public String addAwardWinning(
+            @PathParam("id") int id,
+            @PathParam("teamNumber") int teamNumber,
+            @PathParam("grade") int grade,
+            @PathParam("total") int total
+    ){
+        awardWinningMapper.addModel(new AwardWinning(id,teamNumber,grade,total));
+        return "redirect:/AWTable";
+    }
+
+    @RequestMapping("addawardwinning")
+    public String toAddAwardWinning(){
+        return "addawardwinning";
+    }
+
+    @RequestMapping("/update_awardwinning")
+    public String updateTransit(
+            @PathParam("id") int id,
+            Model model
+    ){
+        AwardWinning model1 = awardWinningMapper.getModel(id);
+        model.addAttribute("aw",model1);
+        return "updateawardwinning";
+    }
+
+    @RequestMapping("/updateAwardWinning")
+    public String updateAwardWinning(
+            @PathParam("id") int id,
+            @PathParam("teamNumber") int teamNumber,
+            @PathParam("grade") int grade,
+            @PathParam("total") int total
+    ){
+        awardWinningMapper.updateModel(new AwardWinning(id,teamNumber,grade,total));
+        return "redirect:/AWTable";
+    }
 }
